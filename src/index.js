9c8bf380f32b38c7020d9faea833103ac2231118
Patrick <patrickbarbosa@ice.ufjf.br>
2020-08-15 10:07:45 -0300

    Configure project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/css/global.css';
+import App from './layout/Main.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
