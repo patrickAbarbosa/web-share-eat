e9ac6a604b5afd652e81e51a3e57d5fdafe4290a
Patrick <patrickbarbosa@ice.ufjf.br>
2020-08-16 18:30:27 -0300

    Render places in home view

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,59 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+// Materia-UI
+import {
+  Grid
+} from '@material-ui/core'
+
+// Core Components
+import CardPlace from '../../components/CardPlace'
+
+const newDist = (placeId, history) => {
+  history.push(`/place/newDist?place=${placeId}`)
+}
+
+const viewPlace = (placeId, history) => {
+  history.push(`/place?place=${placeId}`)
+}
+
+const renderPlaces = (places, history) => {
+  
+  if(!places || !places?.length > 0)
+    return <h3>Nenhum lugar encontrado.</h3>
+
+  return places.map((place, indice) => {
+    console.log(place)
+    return (
+      <Grid item xs={12} sm={6} md={4} lg={4}>
+        <CardPlace 
+          title={place.name} 
+          onClickPlace={() => viewPlace(indice, history)}
+          onClickAdd={() => newDist(indice, history)}
+        >
+          {place?.menuItems?.length} pratos
+        </CardPlace>
+      </Grid>
+    )
+  })
+}
 
 const Home = (props) => {
-  console.log(props)
+
+  const { places } = props.place
+  
   return (
-    <div>
+    <>
       <h1>Lugares</h1>
-    </div>
+      <span>{places?.length} lugares cadastrados</span>
+      <Grid style={{marginTop: '0.5em'}} container spacing={2}>
+        {renderPlaces(places, props.history)}
+      </Grid>
+    </>
   )
 }
 
-export default Home
+const mapStateToProps = ({ place }) => {
+  return { place }
+}
+export default connect(mapStateToProps)(Home)
