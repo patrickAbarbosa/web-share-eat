3dcd6f88f87a23e07724eaa184f1ad8f5e9547d3
Patrick <patrickbarbosa@ice.ufjf.br>
2020-08-16 21:45:09 -0300

    Add button add and back in Place view

--- src/views/Place/Place.jsx
+++ src/views/Place/Place.jsx
@@ -3,11 +3,16 @@ import { connect } from 'react-redux'
 
 // Materia-UI
 import {
-  Grid
+  Grid,
+  IconButton,
+  makeStyles
 } from '@material-ui/core'
+
+import { AddCircle, NavigateBefore } from '@material-ui/icons'
 
 // Core Components
 import CardMenuItem from '../../components/CardMenuItem'
+import env from '../../env'
 
 // Actions 
 import { query } from '../../utils/functions'
@@ -28,21 +33,43 @@ const renderMenu = (menu, history) => {
   })
 }
 
+const styles = {
+  addButton: makeStyles({
+    colorPrimary: {
+      color: env.colors.primary
+    }
+  })
+}
+
 const Place = (props) => {
   const [search, setSearch] = useState(null)
   const { places } = props.place
+  
   useEffect(() => {
     setSearch(query(props.location))
   }, [])
-  console.log(props)
+  
   return (
     search?.place ?
     <>
+      <div style={{position: 'absolute', top: '0.5em', marginLeft: '-1em'}}>
+        <IconButton aria-label="Ver mais" onClick={() => props.history.goBack()}>
+          <NavigateBefore fontSize="large" />
+        </IconButton>
+      </div>
+
       <h1>{places[search.place]?.name}</h1>
       <span>{places[search.place]?.menuItems?.length} pratos</span>
+      
       <Grid style={{marginTop: '0.5em'}} container spacing={2}>
         {renderMenu(places[search.place]?.menuItems)}
       </Grid>
+      
+      <div style={{position: 'absolute', bottom: '0.5em', right: '0.5em'}}>
+        <IconButton aria-label="Ver mais" onClick={() => props.history.push(`/place/newDist?place=${search.place}`)}>
+          <AddCircle fontSize="large"  style={{ color: env.colors.primary, fontSize: '2.5em'}} />
+        </IconButton>
+      </div>
     </>
     : 
     <>
